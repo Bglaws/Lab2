0df04b4862902a449f22296966865440fd8b1f72
Bglaws <glaws.brian@gmail.com>
2021-07-23 16:18:32 -0400

    updated script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 echo "all documentaries from movies.csv"
 echo "-----------------------------------------------------------------"
-sort -t "\n" | awk '/Documentary/ {print}' movies.csv
+awk '/Documentary/ {print}' movies.csv
 echo "-----------------------------------------------------------------"
 echo "total number of Documentaries: " 
 awk 'END{print FNR}' movies.csv 
